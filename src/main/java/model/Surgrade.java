bfae1bcf9ea0dbd56db8f0cf3975e4cc424c0b6b
LéoMeurdra <ts1sio@SIO301-07.sio.bts>
2024-03-28 13:24:32 +0100

    Création de la classe métier Surgrade

---
+++ src/main/java/model/Surgrade.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class Surgrade {
+    
+    private int id;
+    private String libelle;
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+}
