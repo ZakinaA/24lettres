429613efe5e4ab9084199b6e6dcfb00060382d96
BocquetMatheo <ts1sio@SIO301-05.sio.bts>
2024-03-28 10:39:30 +0100

    création du test TestFonction.java

---
+++ src/main/java/test/TestFonction.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import model.Fonction;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class TestFonction {
+     public static void main (String args[]){
+        
+        Fonction f = new Fonction();
+        f.setId(1);
+        f.setLibelle("Fonction1");
+        
+    }
+}
