61cc46c6f97f9938d5d7784955bef8a8d63148c2
BocquetMatheo <ts1sio@SIO301-05.sio.bts>
2024-03-28 15:39:16 +0100

    ajout de la relation One To Many

--- src/main/java/test/TestVehicule.java
+++ src/main/java/test/TestVehicule.java
@@ -6,6 +6,7 @@ package test;
 
 import java.time.LocalDate;
 import model.Vehicule;
+import model.TypeVehicule;
 
 /**
  *
@@ -21,8 +22,17 @@ public class TestVehicule {
         vehicule1.setDateOrigine(LocalDate.of(1995, 12, 5));
         vehicule1.setDateRevision(LocalDate.of(2022, 1, 3));
         
-        System.out.println("Le véhicule n°" + vehicule1.getId() + " est un " + vehicule1.getImmat()+ ", existe depuis " + vehicule1.getDateOrigine() + " et doit faire une révision en " + vehicule1.getDateRevision());
+        
+        
+        TypeVehicule typeVehicule1 = new TypeVehicule();
+        typeVehicule1.setId(1);
+        typeVehicule1.setNom("VSAV");
+        typeVehicule1.setCaracteristique("véhicule de secours aux victimes");
+        
+        vehicule1.setTypeVehicule(typeVehicule1);
+        
+        System.out.println("Le véhicule n°" + vehicule1.getId() + " est un " + vehicule1.getImmat()+ ", existe depuis " + vehicule1.getDateOrigine() + " et doit faire une révision en " + vehicule1.getDateRevision() + ", son type de véhicule est "+ vehicule1.getTypeVehicule().getNom());
         
     }
+    }
     
-}
