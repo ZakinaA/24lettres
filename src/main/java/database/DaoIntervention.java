c31d15e824ae5528452d369d425e740be084d543
mathe <mathe@Matho>
2024-04-05 22:34:41 +0200

    Correction requete sql getLesPompierByIntervention

--- src/main/java/database/DaoIntervention.java
+++ src/main/java/database/DaoIntervention.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import model.Fonction;
 import model.Intervention;
 import model.Pompier;
 
@@ -27,21 +28,23 @@ public class DaoIntervention {
         
         ArrayList<Intervention> lesInterventions = new ArrayList<Intervention>();
         try{
-            requeteSql = cnx.prepareStatement("SELECT intervention.id AS i_id, intervention.lieu AS i_lieu, intervention.date AS i_date, intervention.heureAppel AS i_heureAppel, intervention.heureArrivee AS i_heureArrivee, intervention.duree AS i_duree FROM intervention INNER JOIN pompier_intervention ON intervention.id = pompier_intervention.intervention_id INNER JOIN pompier ON pompier.id = pompier_intervention.pompier_id;");
+            requeteSql = cnx.prepareStatement("SELECT intervention.id AS i_id, intervention.lieu AS i_lieu, intervention.date AS i_date, intervention.heureAppel AS i_heureAppel, intervention.heureArrivee AS i_heureArrivee, intervention.duree AS i_duree "
+                                            + "FROM intervention");
             resultatRequete = requeteSql.executeQuery();
             
-           while (resultatRequete.next()) {
-            Intervention i = new Intervention();
-            i.setId(resultatRequete.getInt("i_id"));
-            i.setLieu(resultatRequete.getString("i_lieu"));
-            
-            Date sqlDate = resultatRequete.getDate("i_date");
-            i.setDate(sqlDate.toLocalDate());
+            while (resultatRequete.next()){
+                
+                Intervention i = new Intervention();
+                i.setId(resultatRequete.getInt("i_id"));
+                i.setLieu(resultatRequete.getString("i_lieu"));
+                 
+                Date datee = resultatRequete.getDate("i_date");
+                i.setDate(datee.toLocalDate());
                     
-            i.setHeureAppel(resultatRequete.getString("i_heureAppel"));
-            i.setHeureArrivee(resultatRequete.getString("i_heureArrivee"));
-            i.setDuree(resultatRequete.getString("i_duree"));
-             
+                i.setHeureAppel(resultatRequete.getString("i_heureAppel"));
+                i.setHeureArrivee(resultatRequete.getString("i_heureArrivee"));
+                i.setDuree(resultatRequete.getString("i_duree"));
+                    
                 
                 lesInterventions.add(i);
             }
@@ -49,7 +52,7 @@ public class DaoIntervention {
         }
         catch (SQLException e){
             e.printStackTrace();
-            System.out.println("La requête de getLesFonctions e généré une erreur");
+            System.out.println("La requête de getLesInterventions a généré une erreur");
         }
         return lesInterventions;
     }
@@ -58,30 +61,36 @@ public class DaoIntervention {
         
         Intervention i = null ;
         try{
-            requeteSql = cnx.prepareStatement("SELECT intervention.id AS i_id, intervention.lieu AS i_lieu, intervention.date AS i_date, intervention.heureAppel AS i_heureAppel, intervention.heureArrivee AS i_heureArrivee, intervention.duree AS i_duree FROM intervention INNER JOIN pompier_intervention ON intervention.id = pompier_intervention.intervention_id INNER JOIN pompier ON pompier.id = pompier_intervention.pompier_id where intervention.id = ?;");
+            requeteSql = cnx.prepareStatement("SELECT intervention.id AS i_id, intervention.lieu AS i_lieu, intervention.date AS i_date, intervention.heureAppel AS i_heureAppel, intervention.heureArrivee AS i_heureArrivee, intervention.duree AS i_duree, pompier.id AS p_id, pompier.nom AS p_nom, pompier.prenom AS p_prenom "
+                                                + "FROM intervention "
+                                                + "INNER JOIN pompier_intervention "
+                                                + "ON intervention.id = pompier_intervention.intervention_id "
+                                                + "INNER JOIN pompier "
+                                                + "ON pompier.id = pompier_intervention.pompier_id "
+                                                + "where intervention.id= ? ");
             requeteSql.setInt(1, idIntervention);
             resultatRequete = requeteSql.executeQuery();
             
-            if (resultatRequete.next()){
+            while (resultatRequete.next()){
                 
-            i = new Intervention();
-            i.setId(resultatRequete.getInt("i_id"));
-            i.setLieu(resultatRequete.getString("i_lieu"));
-            
-            Date sqlDate = resultatRequete.getDate("i_date");
-            i.setDate(sqlDate.toLocalDate());
+                i = new Intervention();
+                i.setId(resultatRequete.getInt("i_id"));
+                i.setLieu(resultatRequete.getString("i_lieu"));
+                 
+                Date datee = resultatRequete.getDate("i_date");
+                i.setDate(datee.toLocalDate());
                     
-            i.setHeureAppel(resultatRequete.getString("i_heureAppel"));
-            i.setHeureArrivee(resultatRequete.getString("i_heureArrivee"));
-            i.setDuree(resultatRequete.getString("i_duree"));
+                i.setHeureAppel(resultatRequete.getString("i_heureAppel"));
+                i.setHeureArrivee(resultatRequete.getString("i_heureArrivee"));
+                i.setDuree(resultatRequete.getString("i_duree"));
+                 
              
-              
             }
            
         }
         catch (SQLException e){
             e.printStackTrace();
-            System.out.println("La requête de getInterventionById  a généré une erreur");
+            System.out.println("La requête de getFonctionById  a généré une erreur");
         }
         return i ;
     }
@@ -96,7 +105,7 @@ public class DaoIntervention {
             requeteSql.setInt(1, idIntervention);
             resultatRequete = requeteSql.executeQuery();
         
-        if (resultatRequete.next()){
+        while (resultatRequete.next()){
             
             p = new Pompier();
             p.setId(resultatRequete.getInt("p_id"));
@@ -110,42 +119,30 @@ public class DaoIntervention {
         
         catch (SQLException e){
             e.printStackTrace();
-            System.out.println("La requête de getInterventionById  a généré une erreur");
+            System.out.println("La requête de getFonctionById  a généré une erreur");
         }
         return lesPompiers;
     }
     
-    public static Intervention addIntervention(Connection connection, Intervention i){      
-        int idGenere = -1;
-        try
-        {
-            //preparation de la requete
-            // id (clé primaire de la table client) est en auto_increment,donc on ne renseigne pas cette valeur
-            // la paramètre RETURN_GENERATED_KEYS est ajouté à la requête afin de pouvoir récupérer l'id généré par la bdd (voir ci-dessous)
-            // supprimer ce paramètre en cas de requête sans auto_increment.
-            requeteSql=connection.prepareStatement("INSERT INTO Intervention (lieu, date, heureAppel, heureArrivee, duree)\n" +
-                    "VALUES (?, ?, ?, ?, ?)", requeteSql.RETURN_GENERATED_KEYS );
-            requeteSql.setString(1, i.getLieu());
-            requeteSql.setDate(2, Date.valueOf(i.getDate()));
-            requeteSql.setString(3, i.getHeureAppel());
-            requeteSql.setString(4, i.getHeureArrivee());
-            requeteSql.setString(5, i.getDuree());
-
-           /* Exécution de la requête */
-            requeteSql.executeUpdate();
+    
+    public static Intervention getNomInterventionById(Connection cnx, int idIntervention){
+        
+        Intervention i = null;
+        
+        try{
+            requeteSql = cnx.prepareStatement("SELECT intervention.id AS i_id, intervention.lieu AS i_lieu "
+                    + "FROM intervention "
+                    + "WHERE intervention.id = ?;");
+            requeteSql.setInt(1, idIntervention);
+            resultatRequete = requeteSql.executeQuery();
             
-             // Récupération de id auto-généré par la bdd dans la table client
-            resultatRequete = requeteSql.getGeneratedKeys();
-            while ( resultatRequete.next() ) {
-                idGenere = resultatRequete.getInt( 1 );
-                i.setId(idGenere);
-                
-                i = DaoIntervention.getInterventionById(connection, i.getId());
+            while (resultatRequete.next()){
+                i = new Intervention();
+                i.setId(resultatRequete.getInt("i_id"));
+                i.setLieu(resultatRequete.getString("i_lieu"));
             }
-            
-         
-        }   
-        catch (SQLException e) 
+        }
+          catch (SQLException e) 
         {
             e.printStackTrace();
             //out.println("Erreur lors de l’établissement de la connexion");
